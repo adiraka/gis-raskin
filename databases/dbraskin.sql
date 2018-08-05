0a040877a51661876978a86f34c17356b509d33f
Adi Raka Siwi <adiraka8@gmail.com>
2018-08-05 16:30:53 +0700

    asdasD

---
+++ databases/dbraskin.sql
@@ -0,0 +1,111 @@
+/*
+SQLyog Ultimate v12.4.3 (64 bit)
+MySQL - 10.1.32-MariaDB : Database - dbraskin
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*Table structure for table `bantuan` */
+
+DROP TABLE IF EXISTS `bantuan`;
+
+CREATE TABLE `bantuan` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `jenis_bantuan` varchar(50) NOT NULL,
+  `banyak_bantuan` int(11) NOT NULL,
+  `satuan` enum('kg','liter') NOT NULL,
+  `nominal` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `bantuan` */
+
+/*Table structure for table `pemberian` */
+
+DROP TABLE IF EXISTS `pemberian`;
+
+CREATE TABLE `pemberian` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `bulan` enum('1','2','3','4','5','6','7','8','9','10','11','12') NOT NULL,
+  `tahun` int(4) NOT NULL,
+  `penerima_id` int(11) NOT NULL,
+  `bantuan_id` int(11) NOT NULL,
+  `tanggal` date NOT NULL,
+  `user_akun_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `pemberian` */
+
+/*Table structure for table `penerima` */
+
+DROP TABLE IF EXISTS `penerima`;
+
+CREATE TABLE `penerima` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `no_kk` varchar(16) NOT NULL,
+  `kepala_keluarga` varchar(50) NOT NULL,
+  `alamat` text NOT NULL,
+  `telepon` varchar(13) NOT NULL,
+  `rt_id` int(11) NOT NULL,
+  `bantuan_id` int(11) NOT NULL,
+  `latitude` float NOT NULL,
+  `longitude` float NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `penerima` */
+
+/*Table structure for table `rt` */
+
+DROP TABLE IF EXISTS `rt`;
+
+CREATE TABLE `rt` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `rw_id` int(11) NOT NULL,
+  `ketua_rt` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `rt` */
+
+/*Table structure for table `rw` */
+
+DROP TABLE IF EXISTS `rw`;
+
+CREATE TABLE `rw` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nama_rw` varchar(3) NOT NULL,
+  `ketua_rw` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `rw` */
+
+/*Table structure for table `user_akun` */
+
+DROP TABLE IF EXISTS `user_akun`;
+
+CREATE TABLE `user_akun` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(20) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `hak_akses` enum('admin','staff') NOT NULL,
+  `status` tinyint(4) NOT NULL DEFAULT '0',
+  `nama` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `user_akun` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
